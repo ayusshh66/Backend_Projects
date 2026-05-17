1b42847b3209631c7572cd1d2aa97c979c9c677d
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-17 16:30:56 +0530

    made fonted for chatting, set up backend

---
+++ chatapp/backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import {Server}   from 'socket.io'
+import cors from 'cors'
+
+const app = express();
+const server = createServer(app);
+app.use(cors())
+const io = new Server(server, {
+    cors : {
+        origin : 'http://localhost:5173',
+        methods : ["GET", "POST"],
+    },
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit('recieve_message',data);
+
+  socket.on("join_room", (data) =>{
+    socket.join(data);
+  })  
+  })
+});
+
+app.get('/', async (req,res) =>{
+    res.status(200).json(`the server is ready`)
+})
+
+server.listen(8000, () => {
+    console.log(`the server is up and running at ${8000} `);
+});
